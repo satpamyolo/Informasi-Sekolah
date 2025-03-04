dbe2efce95c67a32a905b3068ccef8bb973d3031
satpamyolo <119924485+satpamyolo@users.noreply.github.com>
2025-03-04 11:04:46 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect('localhost','root','','sma1');
+$koneksi = mysqli_connect('localhost','root','','datasekolah');
 
 if (!$koneksi){
     die('Gagal terkoneksi ke database' . mysqli_connect_error());
